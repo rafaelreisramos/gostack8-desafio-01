7e4fdda570521d7b64dab529eaca09ce676e6de6
rafaelreisramos <rrramos@gmail.com>
2019-09-25 14:22:22 -0300

    Project creation POST route

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+const projects = [];
+
 server.get('/', (req, res) => {
   return res.send('Hello World');
+});
+
+server.post('/projects', (req, res) => {
+  const { id, project } = req.body;
+
+  projects.push({ id: id, project: project, tasks: [] });
+
+  return res.json( projects );
 });
 
 server.listen(3000);
