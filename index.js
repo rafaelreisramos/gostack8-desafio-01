5e40846079a37d860681e579fa94212aaff2bc49
rafaelreisramos <rrramos@gmail.com>
2019-09-26 09:36:45 -0300

    Added a middleware to check if project exists

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ server.use(express.json());
 
 const projects = [];
 
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+
+  const index = projects.findIndex(item => item.id === id);
+
+  if (index < 0) {
+    return res.status(400).json({ error: 'Project does not exists' });
+  } 
+
+  req.projectId = index;
+
+  return next();
+}
+
 server.post('/projects', (req, res) => {
   const { id, title } = req.body;
 
@@ -18,34 +32,34 @@ server.get('/projects', (req, res) => {
   return res.json( projects );
 })
 
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', checkProjectExists, (req, res) => {
   const { id } = req.params;
 
   const newProjectName = req.body.title;
   
-  const index = projects.findIndex(item => item.id === id);
+  const index = req.projectId;
 
   projects[index].title = newProjectName;
 
   return res.json( projects );
 })
 
-server.delete('/projects/:id', (req, res) => {
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
   const { id } = req.params;
   
-  const index = projects.findIndex(item => item.id === id);
+  const index = req.projectId;
 
   projects.splice(index, 1);
 
   return res.json( projects );
 })
 
-server.post('/projects/:id/tasks', (req, res) => {
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
   const { id } = req.params;
 
   const taskName = req.body.title;
 
-  const index = projects.findIndex(item => item.id === id);
+  const index = req.projectId;
 
   projects[index].tasks.push(taskName);
 
